b97d7c02588951bf533d4bb27e6b909a15311690
a3magic3pocket <a3magic3pocket@gmail.com>
2025-02-27 13:29:17 +0900

    sql 업데이트.

--- src/main/resources/sql/ddl.sql
+++ src/main/resources/sql/ddl.sql
@@ -13,3 +13,22 @@ CREATE TABLE IF NOT EXISTS `user` (
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP
 );
+
+-- SERVICE_LEADER_LOCK 테이블 생성
+CREATE TABLE IF NOT EXISTS `service_leader_lock` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name` varchar(100) NOT NULl,
+  `container_id` varchar(100) DEFAULT NULL,
+  `timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   INDEX `service_leader_lock-name` (`name`)  -- 인덱스를 추가
+)
+;
+
+-- KEY_VALUE_STORE 테이블 생성
+CREATE TABLE IF NOT EXISTS `key_value_store` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `key` varchar(100) NOT NULl UNIQUE,
+  `value` TEXT NOT NULL,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+)
+;
